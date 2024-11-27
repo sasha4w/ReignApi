94a39d3f05ed6a2a0d8cf4a869ce108dac8db9f9
Sasha Rochedix <sasharochedix@gmail.com>
2024-11-27 12:03:27 +0100

    suppression des controller et route de base du squelette de l'api

--- src/routes.php
+++ src/routes.php
@@ -8,16 +8,6 @@ les routes
  */
 
 return [
-    // ['', '/client-php/curl', 'curl@index'],
-    
-    ['GET', '/api/v1/fake-datas/posts', 'post@index'],
-
-    ['GET', '/api/v1/fake-datas/posts/{id:\d+}', 'post@show'],
-    
-    ['POST', '/api/v1/fake-datas/posts', 'post@create'],  
-    ['PATCH', '/api/v1/fake-datas/posts/{id:\d+}', 'post@update'],  
-    ['DELETE', '/api/v1/fake-datas/posts/{id:\d+}', 'post@delete'],  
-
     ///// ADMIN /////
 
     /// GET ///
